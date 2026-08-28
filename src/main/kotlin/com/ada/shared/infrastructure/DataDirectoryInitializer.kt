f215ba64b897bdb26851b36fb11a999b49b91b84
FedeMarkoo <fedemarkoo@users.noreply.github.com>
2026-08-28 01:33:37 -0300

    fix: allow Spring configuration proxying

--- src/main/kotlin/com/ada/shared/infrastructure/DataDirectoryInitializer.kt
+++ src/main/kotlin/com/ada/shared/infrastructure/DataDirectoryInitializer.kt
@@ -8,7 +8,7 @@ import java.nio.file.Files
 import java.nio.file.Path
 
 @Configuration
-class DataDirectoryInitializer(
+open class DataDirectoryInitializer(
     @Value("\${ada.data-dir:../ada-data}") private val dataDirectory: String,
 ) {
     @Bean
